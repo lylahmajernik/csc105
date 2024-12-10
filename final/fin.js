e909548ba77bf86c29044eb323167e493db25a39
lylahmajernik <lhmajernik@live.carlow.edu>
2024-12-10 18:21:06 -0500

    Add files via upload

---
+++ final/fin.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	
+	
+	
+	
+	
+			$(".button-53").click(function() {
+				$(this).next("p").toggle();
+		});
+		
+			$("li").hover(function() {
+				$(this).animate({fontSize: '1.5em'},200);
+			}, function() {
+				$(this).animate({fontSize: '1em'},200);
+				});
+				
+			$("img").click(function() {
+				$(this).fadeToggle("slow", "linear");
+			});
+			
+			$("#imlylah").click(function() {
+				$("#profpic").fadeToggle("slow", "linear");
+			});
+});
